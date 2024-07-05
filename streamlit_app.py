827af4ee2adb6fe608e9440708bdf4274948f026
BritneySandon <148301379+Br0tnie@users.noreply.github.com>
2024-07-04 21:14:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "SAS [docs.streamlit.io](https://docs.streamlit.io/)."
+
 )
+st.subheader("Selecciona tu tipo de alimentación")
+tipo_alimentacion = st.selectbox("Tipo de alimentación", ["Omnívoro", "Vegetariano", "Vegano", "Celíaco", "Diabético"])
