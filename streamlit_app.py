368388879ed3719254a7e63c652b4ed426f9dd2f
BritneySandon <148301379+Br0tnie@users.noreply.github.com>
2024-07-04 21:20:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("PrepararIA: Caso de uso 1.")
 st.write(
     "SAS [docs.streamlit.io](https://docs.streamlit.io/)."
 
 )
+# Sección para seleccionar tipo de alimentación
 st.subheader("Selecciona tu tipo de alimentación")
 tipo_alimentacion = st.selectbox("Tipo de alimentación", ["Omnívoro", "Vegetariano", "Vegano", "Celíaco", "Diabético"])
+
+# Sección para seleccionar tipo de preparación
+st.subheader("Selecciona el tipo de preparación")
+tipo_preparacion = st.selectbox("Tipo de preparación", ["Desayuno", "Cena", "Postre", "Almuerzo"])
